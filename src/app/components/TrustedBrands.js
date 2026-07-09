ef6548c41b08173cb28d30baad94772535c42add
TonmoyZohani <tonmoyzohani@gmail.com>
2026-07-09 20:37:43 +0600

    update

--- src/app/components/TrustedBrands.js
+++ src/app/components/TrustedBrands.js
@@ -29,7 +29,7 @@ export default function TrustedBrands() {
       <div className="container mx-auto px-4 mb-8 text-center">
         {/* <h2 className="text-4xl md:text-5xl font-bold text-center text-white mb-2">SUPPORTED BY </h2> */}
         <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4">
-          SUPPORTED{" "}
+          CERTIFIED{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-purple-400">
             BY
           </span>
